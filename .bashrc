aca4164021b80834c530011ba25756f488a7bded
impulse74 <potkanzjedolsnickers@gmail.com>
2025-03-14 18:34:08 +0100

    updates

--- .bashrc
+++ .bashrc
@@ -7,7 +7,8 @@
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias bc='bc -l'
+alias vvim='/usr/bin/vim'
+alias vim='nvim'
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 PS1='\[\e[32m\]\u@\h\[\e[0m\]\[\e[34m\] \W \$ \[\e[0m\]'
 
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,38 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+-- Line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Smart indentation
+vim.o.smartindent = true
+
+-- Tab width
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+-- Use spaces instead of tabs
+vim.o.expandtab = true
+
+-- Ignore case when searching
+vim.o.ignorecase = true
+-- Override ignorecase if the search contains uppercase letters
+vim.o.smartcase = true
+-- Incremental search
+vim.o.incsearch = true
+-- Highlight search results
+vim.o.hlsearch = true
+
+-- Line wrapping
+vim.o.wrap = true
+
+-- Always show tab line
+vim.o.showtabline = 1
+
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>h", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>tn", ":tabnew | Explore<CR>", { silent = true });
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { silent = true });
+vim.keymap.set("v", "♠", '"+y')
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -28,37 +56,54 @@ require("lazy").setup({
     spec = {
         {
             'maxmx03/solarized.nvim',
-            lazy = false,
-            priority = 1000,
-            ---@type solarized.config
-            opts = {},
-            config = function(_, opts)
-                vim.o.termguicolors = true
+            lazy = false, -- make sure we load this during startup if it is your main colorscheme
+            priority = 1000, -- make sure to load this before all the other start plugins
+            config = function()
+                vim.cmd.colorscheme "solarized"
                 vim.o.background = 'dark'
-                require('solarized').setup(opts)
-                vim.cmd.colorscheme 'solarized'
+                require('solarized').setup({
+                    variant = 'spring', -- "spring" | "summer" | "autumn" | "winter" (default)
+                })
             end,
         },
         {
-            'nvim-telescope/telescope.nvim', 
-            dependencies = { 'nvim-lua/plenary.nvim' },
+            'nvim-lualine/lualine.nvim',
+            dependencies = { 'nvim-tree/nvim-web-devicons' },
+            config = function()
+                require("lualine").setup({})
+            end,
+        },
+        {
+            "nvim-telescope/telescope.nvim", 
+            dependencies = { "nvim-lua/plenary.nvim" },
             config = function()
                 require("telescope").setup({})
 
-                local builtin = require('telescope.builtin')
-                vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-                vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-                vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-                vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+                local builtin = require("telescope.builtin")
+                vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+                vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+                vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+                vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
             end,    
         },
         {
             "nvim-treesitter/nvim-treesitter",
             build = ":TSUpdate",
             config = function()
-                require("nvim-treesitter") 
+                local config = require("nvim-treesitter.configs")
+                config.setup({
+                    ensure_installed = { "lua", "c", "cpp", "cmake", "rust", "asm", "python", "bash", "python" },
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                })
             end,
         },
+        {
+            "windwp/nvim-autopairs",
+            event = "InsertEnter",
+            config = true,
+        },
+
     },
     install = { colorscheme = { "solarized" } },
     -- automatically check for plugin updates
